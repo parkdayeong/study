a47bedd83ab4ebbbe603e25262a6d6ba0810e107
parkdayeong <molra0000@naver.com>
2023-10-25 16:12:49 +0900

    class101 add folder

--- javascript/opentutorials/colors.js
+++ javascript/opentutorials/colors.js
@@ -1,32 +1,36 @@
 var Links = {
-  setColor : function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while (i < alist.length) {
-      alist[i].style.color = color;
-      i = i + 1;
+    setColor: function (color) {
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while (i < alist.length) {
+        //   alist[i].style.color = color;
+        //   i = i + 1;
+        // }
+        $('a').css("color", color)
     }
-  }
 }
-var Body ={
-  setBackgroundColor : function (color){
-  document.querySelector('body').style.backgroundColor = color},
-  setColor : function(color){
-  document.querySelector('body').style.color = color
-  } 
+var Body = {
+    setBackgroundColor: function (color) {
+        // document.querySelector('body').style.backgroundColor = color},
+        $('body').css("background-color", color)
+    },
+    setColor: function (color) {
+        //   document.querySelector('body').style.color = color
+        $('body').css("color", color)
+    }
 
 }
 
 function nightDayhandler(self) {
-  if (self.value === 'night') {
-    Body.setBackgroundColor('black');
-    Body.setColor('white');
-    self.value = 'day';
-    Links.setColor('yellow');
-  } else {
-    Body.setBackgroundColor('white');
-    Body.setColor('black');
-    self.value = 'night';
-    Links.setColor('black');
-  }
+    if (self.value === 'night') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day';
+        Links.setColor('yellow');
+    } else {
+        Body.setBackgroundColor('beige');
+        Body.setColor('black');
+        self.value = 'night';
+        Links.setColor('black');
+    }
 }
