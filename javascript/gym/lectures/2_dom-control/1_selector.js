26e74930be8182b640a2a5ed309fff6fd0f38465
parkdayeong <molra0000@naver.com>
2023-10-30 15:47:47 +0900

    231030 gym coding

---
+++ javascript/gym/lectures/2_dom-control/1_selector.js
@@ -0,0 +1,27 @@
+// [1]get 메서드
+// let title = 드ocument.getElementById("title");
+// console.log(title);
+// title.style.color = "red";
+
+// let items = document.getElementsByClassName("item");
+// console.log(items);
+// console.log(items[0]);
+// console.log(items[1]);
+// console.log(items[2]);
+
+// let liList = document.getElementsByTagName("li");
+// console.log(liList);
+
+// [2] Html요소 쿼리
+let h2 = document.querySelector("h2");
+console.log(h2);
+let h2id = document.querySelector("#title");
+console.log(h2id);
+
+let item = document.querySelector(".item");
+console.log(item);
+let items = document.querySelectorAll(".item");
+console.log(items);
+console.log(items[0]);
+console.log(items[1]);
+console.log(items[2]);
