2504699e321792dc8e3cc163a61baf29b4773775
parkdayeong <molra0000@naver.com>
2023-11-06 09:46:19 +0900

    231106-start

--- javascript/gym/lectures/2_dom-control/ep10.js
+++ javascript/gym/lectures/2_dom-control/ep10.js
@@ -1,1 +1,31 @@
 //ep10
+
+let inputBox = document.querySelector('#input-box');
+
+// inputBox.addEventListener('keydown', function (event) {
+//   console.log('keydown');
+// });
+
+// inputBox.addEventListener('keyup', function (event) {
+//   console.log('keyup');
+// });
+
+// inputBox.addEventListener('keypress', function (event) {
+//   console.log('keypress');
+// });
+
+// inputBox.addEventListener('keypress', function (event) {
+//   console.log('keypress');
+// });
+
+// inputBox.addEventListener('focus', function (event) {
+//   console.log('focus');
+// });
+
+// inputBox.addEventListener('blur', function (event) {
+//   console.log('blur');
+// });
+
+// inputBox.addEventListener('change', function (event) {
+//   console.log('change');
+// });
--- javascript/gym/self/mouseevent.js
+++ javascript/gym/self/mouseevent.js
@@ -1,5 +1,10 @@
-let consoleLogBtn = document.querySelector('#myButton');
+let rect = document.querySelector('.rect');
+console.log(rect);
+let circle = document.querySelector('.circle');
+console.log(circle);
+let body = document.querySelector('body');
 
-consoleLogBtn.addEventListener('click', function () {
-  console.log('hi');
+body.addEventListener('click', function (event) {
+  circle.style.left = event.pageX - 15 + 'px';
+  circle.style.top = event.pageY - 15 + 'px';
 });
