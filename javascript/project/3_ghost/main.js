77ed90e4a6306cfe5cd1f5f20cf36c507a270b40
parkdayeong <molra0000@naver.com>
2023-11-21 17:54:36 +0900

    231121

--- javascript/project/3_ghost/main.js
+++ javascript/project/3_ghost/main.js
@@ -1,9 +1,9 @@
 'use strict';
-
 let body = document.querySelector('body');
 let ghost = document.querySelector('.ghost');
 
+// ghost
 body.addEventListener('mousemove', (e) => {
-  ghost.style.top = e.pageY + 'px';
   ghost.style.left = e.pageX + 'px';
+  ghost.style.top = e.pageY + 'px';
 });
---
+++ vue/1_vuedongsan/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h3>logo영역</h3>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>70 만원</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      // 데이터보관
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    };
+  },
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
