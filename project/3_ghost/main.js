957cc736819c13a6a2f56390e68c6f6b2222ffbb
parkdayeong <molra0000@naver.com>
2023-11-27 10:42:34 +0900

    231127

--- project/3_ghost/main.js
+++ project/3_ghost/main.js
@@ -1,8 +1,8 @@
 'use strict';
+
 let body = document.querySelector('body');
 let ghost = document.querySelector('.ghost');
 
-// ghost
 body.addEventListener('mousemove', (e) => {
   ghost.style.left = e.pageX + 'px';
   ghost.style.top = e.pageY + 'px';
